55e56f2d7dd28c36f5fcba0788fb12ae9842c51c
Antanes <venam.i-com@mail.ru>
2024-01-25 17:06:42 +0500

    List

--- TestApp.Domain/Machine.cs
+++ TestApp.Domain/Machine.cs
@@ -6,8 +6,8 @@ namespace TestApp.Domain
     public class Machine
     {
         public int Id { get; set; }
-        public ICollection<Drink> Drinks { get; set; }
-        public ICollection<Coin> Coins { get; set; }        
+        public List<Drink> Drinks { get; set; }
+        public List<Coin> Coins { get; set; }        
         public int ClientBalance { get; set; }       
         public int Change { get; set; }
         public int Coin1quantity { get; set; }
